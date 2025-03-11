96d65787b6ae22df3f205f6f504a8f08a617b1fa
Farouk Zakaria <gameswithoutlose97@gmail.com>
2025-03-11 19:45:09 +0200

    feat: register Like and Share models in admin panel

--- socialmedia/admin.py
+++ socialmedia/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Post, User, Comment
+from .models import Post, User, Comment, Like, Share
 
 admin.site.register(Post)
 admin.site.register(User)
 admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Share)
+
