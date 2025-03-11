74a2068b1187306c2bdc64afeba0ee9be07003ff
Farouk Zakaria <gameswithoutlose97@gmail.com>
2025-03-11 19:39:51 +0200

    feat: enhance User model with additional fields and constraints

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -2,17 +2,25 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=255, db_index=True)
-    email = models.EmailField(db_index=True)
-    password = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, db_index=True, null=False, blank=False, unique=True)
+    email = models.EmailField(db_index=True, null=False, blank=False, unique=True)
+    password = models.CharField(max_length=255, null=False, blank=False)
+    is_anonymous = models.BooleanField(default=False)
+    is_authenticated = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'password']
 
     def __str__(self):
         return self.username + " - " + self.email
 
 
 class Post(models.Model):
-    user = models.ForeignKey('User', related_name="posts", on_delete=models.CASCADE, db_index=True)
+    user = models.ForeignKey('User', related_name="posts", on_delete=models.CASCADE, db_index=True, default=1)
     title = models.CharField(max_length=255, db_index=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
