5470c1fc6b7ed34b688d5a4901ea8722f008ef61
Farouk Zakaria <gameswithoutlose97@gmail.com>
2025-03-11 17:38:13 +0200

    refactor: reorganize User and Post models, enhance string representations, and add database indexing

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -1,17 +1,9 @@
 from django.db import models
 
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
 
 class User(models.Model):
-    username = models.CharField(max_length=255)
-    email = models.EmailField()
+    username = models.CharField(max_length=255, db_index=True)
+    email = models.EmailField(db_index=True)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -19,33 +11,38 @@ class User(models.Model):
         return self.username + " - " + self.email
 
 
-class Like(models.Model):
-    user = models.ForeignKey('User', related_name="likes", on_delete=models.CASCADE)
-    post = models.ForeignKey('Post', related_name="likes", on_delete=models.CASCADE)
+class Post(models.Model):
+    user = models.ForeignKey('User', related_name="posts", on_delete=models.CASCADE, db_index=True)
+    title = models.CharField(max_length=255, db_index=True)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-    indexes = [
-            models.Index(fields=['user']),
-            models.Index(fields=['post']),
-        ]
+    def __str__(self):
+        return self.title + " by: " + self.user.username
+
+
+class Like(models.Model):
+    user = models.ForeignKey('User', related_name="likes", on_delete=models.CASCADE, db_index=True)
+    post = models.ForeignKey('Post', related_name="likes", on_delete=models.CASCADE, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} liked {self.post.title}"
+        return f"{self.user.username} liked {self.post.title} that is by {self.post.user.username}"
 
 class Share(models.Model):
-    user = models.ForeignKey('User', related_name="shares", on_delete=models.CASCADE)
-    post = models.ForeignKey('Post', related_name="shares", on_delete=models.CASCADE)
+    user = models.ForeignKey('User', related_name="shares", on_delete=models.CASCADE, db_index=True)
+    post = models.ForeignKey('Post', related_name="shares", on_delete=models.CASCADE, db_index=True)
     shared_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user.username} shared {self.post.title}"
+        return f"{self.user.username} shared {self.post.title} that is by {self.post.user.username}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE, db_index=True)
+    user = models.ForeignKey(User, related_name="comments", on_delete=models.CASCADE, db_index=True)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.user.username} on {self.post.title}"
+        return f"Comment by {self.user.username} on {self.post.title} that is by {self.post.user.username}"
